f2aceff2c837c6e80cab271c312f691ccc0d1a6a
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-07-16 18:44:27 -0500

    feat: added debug json

--- backend/db/seeders/20250712202219-dummy-products.js
+++ backend/db/seeders/20250712202219-dummy-products.js
@@ -6,32 +6,32 @@ module.exports = {
     return queryInterface.bulkInsert('products', [
       {
         name: 'Acoustic Guitar',
-        price: 499.99,
+        price_in_cents: 5000000,
         stock: 80,
         description: 'High-quality acoustic guitar with a rich sound.',
       },
       {
         name: 'Electric Guitar',
-        price: 799.99,
+        price_in_cents: 8000000,
         stock: 50,
         description:
           'Versatile electric guitar suitable for various music styles.',
       },
       {
         name: 'Drum Set',
-        price: 1299.99,
+        price_in_cents: 120000000,
         stock: 50,
         description: 'Complete drum set with cymbals and hardware included.',
       },
       {
         name: 'Keyboard',
-        price: 599.99,
+        price_in_cents: 60000000,
         stock: 90,
         description: 'Digital keyboard with weighted keys and multiple sounds.',
       },
       {
         name: 'Violin',
-        price: 899.99,
+        price_in_cents: 90000000,
         stock: 40,
         description: 'Professional violin with a beautiful finish and sound.',
       },
